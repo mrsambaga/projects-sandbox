7e90cfdff38d5784290baaed6702b719287cd9a5
mrsambaga <m.r.sambaga@gmail.com>
2024-07-03 20:57:39 +0700

    create result table component

---
+++ investment-calculator/src/component/ResultTable.jsx
@@ -0,0 +1,16 @@
+export default function ResultTable({ annualData }) {
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+  );
+}
