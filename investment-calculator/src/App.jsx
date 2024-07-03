fd95f1e2993be4c604722c6a85021d1237be5662
mrsambaga <m.r.sambaga@gmail.com>
2024-07-03 20:58:25 +0700

    create 4 input & result table in app jsx

--- investment-calculator/src/App.jsx
+++ investment-calculator/src/App.jsx
@@ -1,7 +1,62 @@
+import { useState } from "react";
+import logo from "./assets/investment-calculator-logo.png";
+import CalculatorInput from "./component/CalculatorInput";
+import ResultTable from "./component/ResultTable";
+import { calculateInvestmentResults } from "./util/investment";
+
 function App() {
+  const [annualData, setAnnualData] = useState([]);
+  const [investmentInput, setInvestmentInput] = useState({
+    initialInvestment: 0,
+    annualInvestment: 0,
+    expectedReturn: 0,
+    duration: 0,
+  });
+
+  const newAnnualData = calculateInvestmentResults(investmentInput);
+
+  function onInputChange(event) {
+    const { name, value } = event.target;
+    const newInput = {
+      ...investmentInput,
+      [name]: value,
+    };
+    setInvestmentInput(newInput);
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <div id="header">
+        <img src={logo}></img>
+        <h1>React Investment Calculator</h1>
+      </div>
+      <div className="input-group">
+        <CalculatorInput
+          inputLabel="INITIAL INVESTMENT"
+          inputName="initialInvestment"
+          onChangeInput={onInputChange}
+        ></CalculatorInput>
+        <CalculatorInput
+          inputLabel="ANNUAL INVESTMENT"
+          inputName="annualInvestment"
+          onChangeInput={onInputChange}
+        ></CalculatorInput>
+        <CalculatorInput
+          inputLabel="EXPECTED RETURN"
+          inputName="expectedReturn"
+          onChangeInput={onInputChange}
+        ></CalculatorInput>
+        <CalculatorInput
+          inputLabel="DURATION"
+          inputName="duration"
+          onChangeInput={onInputChange}
+        ></CalculatorInput>
+      </div>
+      <div>
+        <ResultTable></ResultTable>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
