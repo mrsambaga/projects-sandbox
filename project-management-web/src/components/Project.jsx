697f61b700ec1dc5dcc7735215872dddd3428464
mrsambaga <m.r.sambaga@gmail.com>
2024-08-30 21:35:01 +0700

    refactor project component using static component

--- project-management-web/src/components/Project.jsx
+++ project-management-web/src/components/Project.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Button from "./Button";
+import Task from "./Task";
 
 export default function Project({ activeProject }) {
   return (
@@ -12,6 +14,15 @@ export default function Project({ activeProject }) {
         Start with the basics, finish with advanced knowledge.
       </p>
       <div class="border-t border-gray-300 my-4"></div>
+      <h1 className="text-3xl font-bold text-stone-600 mb-5">Tasks</h1>
+      <div className="w-1/2 flex justify-between h-8 mb-6">
+        <input className="bg-stone-300 w-4/5 p-3"></input>
+        <Button title={"Add Task"} style={"text-lg font-bold text-stone-600"} />
+      </div>
+      <div className="w-1/2 flex flex-col space-y-4 bg-stone-200 p-3">
+        <Task task={"Practice, practice, practice !"} />
+        <Task task={"Learn advance topic"} />
+      </div>
     </div>
   );
 }
