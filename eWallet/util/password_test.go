6fa9fb4f248256083be9cc2855a2d2f90ea2cdf2
mrsambaga <m.r.sambaga@gmail.com>
2024-12-15 15:15:46 +0700

    create utils for hashing & checking password

---
+++ eWallet/util/password_test.go
@@ -0,0 +1,30 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestHashPassword(t *testing.T) {
+	password := RandomString(6)
+
+	hashedPassword, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword)
+}
+
+func TestCheckPassword(t *testing.T) {
+	password := RandomString(6)
+	wrongPassword := RandomString(6)
+
+	hashedPassword, err := HashPassword(password)
+	require.NoError(t, err)
+
+	wrongHashedPassword, err := HashPassword(wrongPassword) 
+	require.NoError(t, err)
+
+	err = CheckPassword(wrongHashedPassword, hashedPassword)
+	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+}
